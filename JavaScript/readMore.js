15f015da11fbe09cdd77e04f7329ccc1e0b47794
niwe <brian.oseguera.om@gmail.com>
2024-06-01 19:14:09 -0600

    Revisión Tarjetas y formularios

--- JavaScript/readMore.js
+++ JavaScript/readMore.js
@@ -16,20 +16,20 @@ let btnAna = document.getElementById("btn-ana");
 let btnBrian = document.getElementById("btn-brian");
 let btnAlexia = document.getElementById("btn-alexia");
 
-let btnState = [false,false,false,false,false,false,false,false];
+let btnState = [false, false, false, false, false, false, false, false];
 
 btnDiana.addEventListener("click", function (event) {
     event.preventDefault();
     if (btnState[0]) {
-        diana.innerHTML=`
-        <p class="desc-txt">Desarrolladora Full Stack, con intereses de integrarme al mundo del
+        diana.innerHTML = `
+        <p class="desc-txt" style="height: 5.4rem;">Desarrolladora Full Stack, con intereses de integrarme al mundo del
         desarrollo web ...</p>`;
-        console.log("texto corto");
+        //console.log("texto corto");
         btnState[0] = false;
     } else {
-        diana.innerHTML=`
-        <p class="desc-txt" style="height: 9rem;">Desarrolladora Full Stack, con intereses de integrarme al mundo del desarrollo web y las TI aprovechando mis habilidades adquiridas a lo largo de mi formación académica.</p>`;
-        console.log("texto largo");
+        diana.innerHTML = `
+        <p class="desc-txt" style="height: 8.7rem;">Desarrolladora Full Stack, con intereses de integrarme al mundo del desarrollo web y las TI aprovechando mis habilidades adquiridas a lo largo de mi formación académica.</p>`;
+        //console.log("texto largo");
         btnState[0] = true;
     }
 });
@@ -37,15 +37,15 @@ btnDiana.addEventListener("click", function (event) {
 btnEsdras.addEventListener("click", function (event) {
     event.preventDefault();
     if (btnState[1]) {
-        esdras.innerHTML=`
-        <p class="desc-txt">El desarrollo web me ha permitido desarrollar persistencia y
+        esdras.innerHTML = `
+        <p class="desc-txt" style="height: 5.4rem;">El desarrollo web me ha permitido desarrollar persistencia y
         resposabilidad personal ...</p>`;
-        console.log("texto corto");
+        //console.log("texto corto");
         btnState[1] = false;
     } else {
-        esdras.innerHTML=`
-        <p class="desc-txt" style="height: 9rem;">El desarrollo web me ha permitido desarrollar persistencia y resposabilidad personal al buscar soluciones creativas mediante la programación y el diseño.</p>`;
-        console.log("texto largo");
+        esdras.innerHTML = `
+        <p class="desc-txt" style="height: 8.7rem;">El desarrollo web me ha permitido desarrollar persistencia y resposabilidad personal al buscar soluciones creativas mediante la programación y el diseño.</p>`;
+        //console.log("texto largo");
         btnState[1] = true;
     }
 });
@@ -53,15 +53,15 @@ btnEsdras.addEventListener("click", function (event) {
 btnAlan.addEventListener("click", function (event) {
     event.preventDefault();
     if (btnState[2]) {
-        alan.innerHTML=`
-        <p class="desc-txt">Desarrollador Java Full Stack Junior con un fuerte interés en la
+        alan.innerHTML = `
+        <p class="desc-txt" style="height: 5.4rem;">Desarrollador Java Full Stack Junior con un fuerte interés en la
         tecnología y la ...</p>`;
-        console.log("texto corto");
+        //console.log("texto corto");
         btnState[2] = false;
     } else {
-        alan.innerHTML=`
-        <p class="desc-txt"style="height: 9rem;">Desarrollador Java Full Stack Junior con un fuerte interés en la tecnología y la programación, busco aportar mi experiencia y habilidades al éxito de proyectos innovadores.</p>`;
-        console.log("texto largo");
+        alan.innerHTML = `
+        <p class="desc-txt"style="height: 8.7rem;">Desarrollador Java Full Stack Junior con un fuerte interés en la tecnología y la programación, busco aportar mi experiencia y habilidades al éxito de proyectos innovadores.</p>`;
+        //console.log("texto largo");   
         btnState[2] = true;
     }
 });
@@ -69,15 +69,15 @@ btnAlan.addEventListener("click", function (event) {
 btnJavier.addEventListener("click", function (event) {
     event.preventDefault();
     if (btnState[3]) {
-        javier.innerHTML=`
-        <p class="desc-txt">Soy un apasionado desarrollador Full Stack Java Jr con estudios
+        javier.innerHTML = `
+        <p class="desc-txt" style="height: 4.2rem;">Soy un apasionado desarrollador Full Stack Java Jr con estudios
         en ingeniería de ...</p>`;
-        console.log("texto corto");
+        //console.log("texto corto");
         btnState[3] = false;
     } else {
-        javier.innerHTML=`
+        javier.innerHTML = `
         <p class="desc-txt" style="height: 11rem;">Soy un apasionado desarrollador Full Stack Java Jr con estudios en ingeniería de software y en ingeniería mecánica.  he adquirido experiencia en el desarrollo de aplicaciones web y tengo habilidades tanto en el frontend como en el backend.</p>`;
-        console.log("texto largo");
+        //console.log("texto largo");
         btnState[3] = true;
     }
 });
@@ -85,15 +85,15 @@ btnJavier.addEventListener("click", function (event) {
 btnJesus.addEventListener("click", function (event) {
     event.preventDefault();
     if (btnState[4]) {
-        jesus.innerHTML=`
-        <p class="desc-txt">Profesional y apasionado realizando los proyectos y
+        jesus.innerHTML = `
+        <p class="desc-txt" style="height: 4.2rem;">Profesional y apasionado realizando los proyectos y
         proyecciones de un trabajo ...</p>`;
-        console.log("texto corto");
+        //console.log("texto corto");
         btnState[4] = false;
     } else {
-        jesus.innerHTML=`
+        jesus.innerHTML = `
         <p class="desc-txt" style="height: 11rem;">Profesional y apasionado  realizando los proyectos y proyecciones  de un trabajo demostrando mejores resultados  quedando a la apertura de seguir conociendo, entusiasmado por lo nuevo.</p>`;
-        console.log("texto largo");
+        //console.log("texto largo");
         btnState[4] = true;
     }
 });
@@ -101,15 +101,15 @@ btnJesus.addEventListener("click", function (event) {
 btnAna.addEventListener("click", function (event) {
     event.preventDefault();
     if (btnState[5]) {
-        ana.innerHTML=`
-        <p class="desc-txt">Cuento con una alta capacidad para resolver problemas de manera
+        ana.innerHTML = `
+        <p class="desc-txt" style="height: 4.2rem;">Cuento con una alta capacidad para resolver problemas de manera
         eficiente y colaborar ...</p>`;
-        console.log("texto corto");
+        //console.log("texto corto");
         btnState[5] = false;
     } else {
-        ana.innerHTML=`
+        ana.innerHTML = `
         <p class="desc-txt" style="height: 11rem;">Cuento con una alta capacidad para resolver problemas de manera eficiente y colaborar con equipos. Busco agregar valor y desarrollar mis habilidades dentro de futuros proyectos.</p>`;
-        console.log("texto largo");
+        //console.log("texto largo");
         btnState[5] = true;
     }
 });
@@ -117,15 +117,15 @@ btnAna.addEventListener("click", function (event) {
 btnBrian.addEventListener("click", function (event) {
     event.preventDefault();
     if (btnState[6]) {
-        brian.innerHTML=`
-        <p class="desc-txt">¡Hola! Soy analista de datos, desarrollador de video-juegos y
+        brian.innerHTML = `
+        <p class="desc-txt" style="height: 4rem;">¡Hola! Soy analista de datos, desarrollador de video-juegos y
         artista digital en ...</p>`;
-        console.log("texto corto");
+        //console.log("texto corto");
         btnState[6] = false;
     } else {
-        brian.innerHTML=`
-        <p class="desc-txt" style="height: 9rem;">¡Hola! Soy analista de datos, desarrollador de video-juegos y artista digital en México. Me gusta la programación creativa para expresarme de forma artística.</p>`;
-        console.log("texto largo");
+        brian.innerHTML = `
+        <p class="desc-txt" style="height: 8.4rem;">¡Hola! Soy analista de datos, desarrollador de video-juegos y artista digital en México. Me gusta la programación creativa para expresarme de forma artística.</p>`;
+        //console.log("texto largo");
         btnState[6] = true;
     }
 });
@@ -133,15 +133,16 @@ btnBrian.addEventListener("click", function (event) {
 btnAlexia.addEventListener("click", function (event) {
     event.preventDefault();
     if (btnState[7]) {
-        alexia.innerHTML=`
-        <p class="desc-txt">Soy Alexia Mata, desarrolladora Fullstack con habilidades para
-        crear aplicaciones ...</p>`;
-        console.log("texto corto");
+        alexia.innerHTML = `
+        <p class="desc-txt" style="height: 4rem;">Soy Alexia Mata, desarrolladora Fullstack con habilidades para crear aplicaciones ...</p>
+        `;
+        //console.log("texto corto");
         btnState[7] = false;
     } else {
-        alexia.innerHTML=`
-        <p class="desc-txt" style="height: 9rem;">Soy Alexia Mata, desarrolladora Fullstack con habilidades para crear aplicaciones web dinámicas y escalables. Me interesan la ciberseguridad y las redes neuronales artificiales</p>`;
-        console.log("texto largo");
+        alexia.innerHTML = `
+        <p class="desc-txt" style="height: 8.4rem;">Soy Alexia Mata, desarrolladora Fullstack con habilidades para crear aplicaciones web dinámicas y escalables. Me interesan la ciberseguridad y las redes neuronales artificiales</p>
+        `;
+        //console.log("texto largo");
         btnState[7] = true;
     }
 });
