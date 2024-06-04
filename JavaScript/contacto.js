08cce38bf8020cae91c500b9716f1bf6fded6a8d
niwe <brian.oseguera.om@gmail.com>
2024-06-03 23:04:03 -0600

    Página de Nosotros y contacto completa con validación y envío a correo

--- JavaScript/contacto.js
+++ JavaScript/contacto.js
@@ -10,6 +10,9 @@ let alertaTexto = document.getElementById("alerta-txt"); //mensaje de error
 
 
 let isValid = true;
+let contador = true;
+let erCorreo = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+let erTelefono = /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/;
 
 
 /// Se da Click al botón de enviar
@@ -22,44 +25,69 @@ btnEnviar.addEventListener("click", function (event) {
     telefono.style.border = "solid 0.01rem #450920";
     comentario.style.border = "solid 0.01rem #450920";
     isValid = true;
-    if (nombre.value.length < 3) { //validación para nombre
+    if (nombre.value.length < 3 && contador == true) { //validación para nombre
         alertaTexto.innerHTML += `<p>El <b style="color: red;">Nombre</b> no es correcto.</p>`;
         alertaValidaciones.style.display = "block";
         nombre.style.border = "solid red medium";
         isValid = false;
-        console.log("not valid");
     }
-    if (correo.value.length < 3) { //validación para correo
+    if (!erCorreo.test(correo.value) && contador == true) { //validación para correo
         alertaTexto.innerHTML += `<p>El <b style="color: red;">Correo electrónico</b> no es correcto.</p>`;
         alertaValidaciones.style.display = "block";
         correo.style.border = "solid red medium";
         isValid = false;
-        console.log("not valid");
     }
-    if (telefono.value.length < 10 || telefono.value.length > 11) { //validación para telefono
+    if (!erTelefono.test(telefono.value) && contador == true) { //validación para telefono
         alertaTexto.innerHTML += `<p>El <b style="color: red;">Teléfono</b> no es correcto.</p>`;
         alertaValidaciones.style.display = "block";
         telefono.style.border = "solid red medium";
         isValid = false;
-        console.log("not valid");
     }
-    if (comentario.value.length < 3) { //validación para comentario
+    if ((comentario.value.length < 4 || comentario.value.length > 150) && contador == true) { //validación para comentario
         alertaTexto.innerHTML += `<p>El <b style="color: red;">Comentario</b> no es correcto.</p>`;
         alertaValidaciones.style.display = "block";
         comentario.style.border = "solid red medium";
         isValid = false;
-        console.log("not valid");
     }
 
-    if (isValid) {
-        //enviamos correo
-        console.log(nombre.value);
-        console.log(correo.value);
-        console.log(telefono.value);
-        console.log(comentario.value);
-        console.log("btn-Enviar");
+    if (isValid && contador) {
+
+        (function () {
+            emailjs.init({
+                publicKey: "cmJyxtBQKn2YS9e4G",
+            });
+        })();
+
+        let serviceID = "service_2rr4m9e";
+        let templateID = "template_vdxekd4";
+
+        params = {
+            nombre_: nombre.value,
+            correo_: correo.value.toLowerCase(),
+            telefono_: telefono.value,
+            comentario_: comentario.value
+        }
+
+
+        //Enviar Correo.
+        emailjs.send(serviceID, templateID, params).then(() => {
+            alertaTexto.innerHTML = ``;
+            alertaValidaciones.style.display = "none";
+            nombre.style.border = "solid 0.01rem #450920";
+            correo.style.border = "solid 0.01rem #450920";
+            telefono.style.border = "solid 0.01rem #450920";
+            comentario.style.border = "solid 0.01rem #450920";
+            nombre.value = '';
+            correo.value = '';
+            telefono.value = '';
+            comentario.value = '';
+            btnEnviar.style.opacity = 0.3;
+            btnEnviar.innerText = "Enviado";
+            contador = false;
+            console.log("prueba para solo mandar un mensaje");
+        }).catch();
+
     }
-    console.log("btn-Enviar");
 
 });
 
