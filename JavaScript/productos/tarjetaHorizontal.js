69a146028fa9500c20d5cb8d28901c5b97ac30eb
Alexia Mata <alexia.i.mata.perez@outlook.com>
2024-06-19 08:41:55 -0600

    Responsive carrusel corregido

--- JavaScript/productos/tarjetaHorizontal.js
+++ JavaScript/productos/tarjetaHorizontal.js
@@ -1,5 +1,5 @@
 function getData() {
-    const promise = fetch('../JSONS/productos.json', {method: 'GET'});
+    const promise = fetch('../JSONS/productos.json', { method: 'GET' });
     promise
         .then((response) => {
             response.json()
@@ -20,24 +20,30 @@ function addCarouselItem(productList) {
     let carouselItem = null;
     let containerItem = null;
     let rowItem = null;
-    let indexCounter = 0; // Este contador nos ayudará a manejar cuando agregar la clase 'active'
+    let indexCounter = 0; 
+    const isMobile = screen.width <= 576;
+    const isTablet = screen.width <= 991
+
+    function createNewCarouselItem() {
+        carouselItem = document.createElement('div');
+        carouselItem.classList.add('carousel-item');
+        if (indexCounter === 0) {
+            carouselItem.classList.add('active');
+        }
+        containerItem = document.createElement('div');
+        containerItem.classList.add('container');
+        rowItem = document.createElement('div');
+        rowItem.classList.add('row');
+
+        containerItem.appendChild(rowItem);
+        carouselItem.appendChild(containerItem);
+        document.getElementById('carousel-container').appendChild(carouselItem);
+    }
 
     productList.forEach((product) => {
-        if (product.discount > 0) {  // Solo crear tarjetas para productos con descuento
-            if (!carouselItem || indexCounter % 3 === 0) {
-                carouselItem = document.createElement('div');
-                carouselItem.classList.add('carousel-item');
-                if (indexCounter === 0) {  // Añadir clase 'active' solo al primer carousel item
-                    carouselItem.classList.add('active');
-                }
-                containerItem = document.createElement('div');
-                containerItem.classList.add('container');
-                rowItem = document.createElement('div');
-                rowItem.classList.add('row');
-
-                containerItem.appendChild(rowItem);
-                carouselItem.appendChild(containerItem);
-                document.getElementById('carousel-container').appendChild(carouselItem);
+        if (product.discount > 0) {
+            if (isMobile || ( isTablet && (!carouselItem || indexCounter % 2 === 0)) || (!carouselItem || indexCounter % 3 === 0)) {
+                createNewCarouselItem();
             }
 
             // Incrementar el contador después de agregar un producto
@@ -46,18 +52,17 @@ function addCarouselItem(productList) {
             // Añadir tarjeta del producto al rowItem
             rowItem.insertAdjacentHTML('beforeend', `
                 <div class="col-sm-12 col-md-4"> <!-- Un solo item por slide en móviles -->
-                    <div class="card mb-7 my-4 rounded-4 justify-content-center" style="width: 100%; height: 320px;"> <!-- Estilo consistente -->
-                        <div class="row g-0">
-                            <div class="col-md-7">  <!-- Imagen a tamaño completo -->
-                                <img src="./src/assets/productssrc/${product.img}" class="img-fluid rounded-start " alt="${product.name}">
+                    <div class="card mb-7 my-4 rounded-4 justify-content-center" style="width: 100%; height: ${ isMobile ? "320px" : "320px"};"> <!-- Estilo consistente -->
+                        <div class="row g-0 align-items-center h-100">
+                            <div class="col-7 h-100 rounded-4" style = "background-image:url(./src/assets/productssrc/${product.img}) !important; background-size: cover; background-position: center;">  <!-- Imagen a tamaño completo -->
                                 <span class="position-absolute top-0 start-0 badge text-bg-danger" style="margin-top: 2rem;">
                                     -${product.discount}%
                                 </span>
                             </div>
-                            <div class="col-md-5"> <!-- Descripción completa -->
+                            <div class="col-5"> <!-- Descripción completa -->
                                 <div class="card-body">
-                                    <h5 class="card-title fs-5">${product.name}</h5>
-                                    <p class="card-text price mt-5">$${product.price - (product.price * product.discount / 100)}  <span class="badge text-bg-danger">-${product.discount}%</span></p>
+                                    <h5 class="card-title fs-4">${product.name}</h5>
+                                    <p class="card-text price mt-md-5 mt-sm-1">$${product.price - (product.price * product.discount / 100)}  <span class="badge text-bg-danger">-${product.discount}%</span></p>
                                     <p class="card-text priceWithoutDiscount" style="text-decoration: line-through;">$ ${product.price}.00</p>
                                     <p class="card-text mt-5" style="display: flex;justify-content: flex-end;">
                                         <a href="#"><img src="./src/assets/shopCart.png" alt="Carrito de compras" style="width: 2rem; margin: 0.2rem 0.4rem;"></a>
