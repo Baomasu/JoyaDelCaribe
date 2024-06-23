c732e240e913fae335d5bf8561c3cc27fd23f004
alangvazq <alan.g.vazq@gmail.com>
2024-06-22 21:19:58 -0600

    Blur agregado

--- JavaScript/registro/registro.js
+++ JavaScript/registro/registro.js
@@ -18,12 +18,22 @@ let confirmPassAlert = document.getElementById("confirmPassAlert")
 
 let userArray = JSON.parse(localStorage.getItem('users')) ? JSON.parse(localStorage.getItem('users')) : [];
 
-
 /* <----------- REGEX ------------> */
-let blankRegex = /^\S*$/; //Regex para espacios en blanco y vacío
-let emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+// Permite guiones y apóstrofes
+let nombreRegex = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s'-]+$/;
+// Solo acepta 10 números y no más de 3 ceros seguidos
+let phoneRegex = /^(?!.*0{3})\d{10}$/;
+// https://emailregex.com/
+let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// Mínimo ocho caracteres, al menos una letra inglesa mayúscula, una letra inglesa minúscula, un número y un carácter especial
 let passRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
-let nombreRegex = /^[ÁÉÍÓÚA-Z][a-záéíóú]+(\s+[ÁÉÍÓÚA-Z]?[a-záéíóú]+)*$/;
+
+formName.addEventListener("blur", () => { validacion(nombreRegex, formName, nameAlert); })
+formLastName.addEventListener("blur", () => { validacion(nombreRegex, formLastName, lastNameAlert); })
+formPhone.addEventListener("blur", () => { validacion(phoneRegex, formPhone, phoneAlert); })
+formEmail.addEventListener("blur", () => { validacion(emailRegex, formEmail, emailAlert); })
+formPassword.addEventListener("blur", () => { validacion(passRegex, formPassword, passAlert); })
+formPasswordConfirm.addEventListener("blur", () => { validacion(passRegex, formPasswordConfirm, confirmPassAlert); })
 
 /// Se da Click al botón de enviar
 formRegistro.addEventListener("submit", validateForm);
@@ -34,65 +44,15 @@ function validateForm(event) {
     let isValid = true;
 
     // Validaciones
+    validacion(nombreRegex, formName, nameAlert);
+    validacion(nombreRegex, formLastName, lastNameAlert);
+    validacion(phoneRegex, formPhone, phoneAlert);
+    validacion(emailRegex, formEmail, emailAlert);
+    validacion(passRegex, formPassword, passAlert);
 
-    if (!nombreRegex.test(formName.value) || formName.value.length < 3) { //validación para formName
-        nameAlert.classList.remove("d-none")
-        formName.classList.add("is-invalid")
-        isValid = false;
-    } else {
-        formName.classList.add("is-valid")
-        formName.classList.remove("is-invalid")
-        nameAlert.classList.add("d-none")
-    }
-
-    if (!nombreRegex.test(formLastName.value) || formLastName.value.length < 3) { //validación para formLastName
-        lastNameAlert.classList.remove("d-none")
-        formLastName.classList.add("is-invalid")
-        isValid = false;
-    } else {
-        formLastName.classList.add("is-valid")
-        formLastName.classList.remove("is-invalid")
-        lastNameAlert.classList.add("d-none")
-    }
-
-    if (formPhone.value.length < 3) { //validación para formPhone
-        phoneAlert.classList.remove("d-none")
-        formPhone.classList.add("is-invalid")
-        isValid = false;
-    } else {
-        formPhone.classList.add("is-valid")
-        formPhone.classList.remove("is-invalid")
-        phoneAlert.classList.add("d-none")
-    }
-
-    if (!emailRegex.test(formEmail.value) || formEmail.value.length < 3 || !blankRegex.test(formEmail.value)) { //validación para formEmail
-        emailAlert.classList.remove("d-none")
-        formEmail.classList.add("is-invalid")
-        isValid = false;
-    } else {
-        formEmail.classList.add("is-valid")
-        formEmail.classList.remove("is-invalid")
-        emailAlert.classList.add("d-none")
-    }
-
-    if (!passRegex.test(formPassword.value) || !blankRegex.test(formPassword.value)) { //validación para formPassword
-        passAlert.classList.remove("d-none")
-        formPassword.classList.add("is-invalid")
-        isValid = false;
-    } else {
-        formPassword.classList.add("is-valid")
-        formPassword.classList.remove("is-invalid")
-        passAlert.classList.add("d-none")
-    }
-
-    if (!blankRegex.test(formPasswordConfirm.value) || formPasswordConfirm.value != formPassword.value) { //validación para formPasswordConfirm
-        confirmPassAlert.classList.remove("d-none")
-        formPasswordConfirm.classList.add("is-invalid")
-        isValid = false;
-    } else {
-        formPasswordConfirm.classList.add("is-valid")
-        formPasswordConfirm.classList.remove("is-invalid")
-        confirmPassAlert.classList.add("d-none")
+    if (formPasswordConfirm.value != formPassword.value) { //validación para formPasswordConfirm
+        
+        validacion(passRegex, formPasswordConfirm, confirmPassAlert);
     }
 
     if (isValid) {
@@ -106,8 +66,6 @@ function validateForm(event) {
         }
         addUser(newUser);
     }
-    // !blankRegex.test(formName.value)
-
 }
 
 function addUser(userObject) {
@@ -118,7 +76,16 @@ function addUser(userObject) {
     localStorage.setItem('userArray', JSON.stringify(userArray));
 }
 
-
-
+function validacion(regex, form, alert) {
+    if (!regex.test(form.value) || form.value.length < 3) {
+        alert.classList.remove("d-none")
+        form.classList.add("is-invalid")
+        return isValid = false;
+    } else {
+        form.classList.add("is-valid")
+        form.classList.remove("is-invalid")
+        alert.classList.add("d-none")
+    }
+}
 
 
