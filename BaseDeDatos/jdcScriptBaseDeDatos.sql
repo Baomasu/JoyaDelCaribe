4968f7d90123b1f0ba69153771b987d4ff388cfc
alangvazq <alan.g.vazq@gmail.com>
2024-07-08 01:35:07 -0600

    Base de datos ajustada a nuevos requerimientos

--- BaseDeDatos/jdcScriptBaseDeDatos.sql
+++ BaseDeDatos/jdcScriptBaseDeDatos.sql
@@ -5,88 +5,94 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema JoyaDelCaribe
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema joyadelcaribe
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema JoyaDelCaribe
+-- Schema joyadelcaribe
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `JoyaDelCaribe` DEFAULT CHARACTER SET utf8 ;
-USE `JoyaDelCaribe` ;
+CREATE SCHEMA IF NOT EXISTS `joyadelcaribe` DEFAULT CHARACTER SET utf8mb3 ;
+USE `joyadelcaribe` ;
 
 -- -----------------------------------------------------
--- Table `JoyaDelCaribe`.`administrador`
+-- Table `joyadelcaribe`.`usuarios`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `JoyaDelCaribe`.`administrador` (
-  `idadministrador` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `correo` VARCHAR(45) NOT NULL,
-  `contraseña` VARCHAR(16) NOT NULL,
-  PRIMARY KEY (`idadministrador`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `JoyaDelCaribe`.`usuarios`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `JoyaDelCaribe`.`usuarios` (
-  `idusuarios` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  `apellido` VARCHAR(45) NOT NULL,
-  `telefono` VARCHAR(10) NOT NULL,
-  `correo` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(16) NOT NULL,
-  PRIMARY KEY (`idusuarios`))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `joyadelcaribe`.`usuarios` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(100) NOT NULL,
+  `apellido` VARCHAR(100) NOT NULL,
+  `telefono` VARCHAR(20) NOT NULL,
+  `correo` VARCHAR(254) NOT NULL,
+  `contrasena` VARCHAR(128) NOT NULL,
+  `rol` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `correo_UNIQUE` (`correo` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `JoyaDelCaribe`.`ordenCompras`
+-- Table `joyadelcaribe`.`orden`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `JoyaDelCaribe`.`ordenCompras` (
-  `idordenCompras` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `usuarios_idusuarios` INT UNSIGNED NOT NULL,
-  `fecha` DATE NULL,
-  `cantidad` INT NULL,
-  `precio` DOUBLE NULL,
-  PRIMARY KEY (`idordenCompras`, `usuarios_idusuarios`),
-  INDEX `fk_ordenCompras_usuarios1_idx` (`usuarios_idusuarios` ASC) VISIBLE,
-  CONSTRAINT `fk_ordenCompras_usuarios1`
-    FOREIGN KEY (`usuarios_idusuarios`)
-    REFERENCES `JoyaDelCaribe`.`usuarios` (`idusuarios`)
+CREATE TABLE IF NOT EXISTS `joyadelcaribe`.`orden` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `fecha` DATE NOT NULL,
+  `total` DECIMAL(10,2) NOT NULL,
+  `usuarios_id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`, `usuarios_id`),
+  INDEX `fk_orden_usuarios1_idx` (`usuarios_id` ASC) VISIBLE,
+  CONSTRAINT `fk_orden_usuarios1`
+    FOREIGN KEY (`usuarios_id`)
+    REFERENCES `joyadelcaribe`.`usuarios` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `JoyaDelCaribe`.`productos`
+-- Table `joyadelcaribe`.`productos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `JoyaDelCaribe`.`productos` (
-  `idproductos` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `product` VARCHAR(250) NOT NULL,
-  `price` DOUBLE NOT NULL,
-  `img` VARCHAR(250) NOT NULL,
-  `category` VARCHAR(250) NOT NULL,
-  `description` VARCHAR(250) NULL,
-  `discount` INT NULL,
-  `administrador_idadministrador` INT UNSIGNED NOT NULL,
-  `ordenCompras_idordenCompras` INT UNSIGNED NOT NULL,
-  `ordenCompras_usuarios_idusuarios` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`idproductos`, `administrador_idadministrador`, `ordenCompras_idordenCompras`, `ordenCompras_usuarios_idusuarios`),
-  INDEX `fk_productos_administrador1_idx` (`administrador_idadministrador` ASC) VISIBLE,
-  INDEX `fk_productos_ordenCompras1_idx` (`ordenCompras_idordenCompras` ASC, `ordenCompras_usuarios_idusuarios` ASC) VISIBLE,
-  CONSTRAINT `fk_productos_administrador1`
-    FOREIGN KEY (`administrador_idadministrador`)
-    REFERENCES `JoyaDelCaribe`.`administrador` (`idadministrador`)
+CREATE TABLE IF NOT EXISTS `joyadelcaribe`.`productos` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(100) NOT NULL,
+  `producto` VARCHAR(250) NOT NULL,
+  `precio` DECIMAL(10,2) NOT NULL,
+  `imagen` VARCHAR(250) NOT NULL,
+  `categoria` VARCHAR(50) NOT NULL,
+  `descripcion` TEXT NULL DEFAULT NULL,
+  `descuento` DECIMAL(5,2) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `joyadelcaribe`.`orden_tiene_productos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `joyadelcaribe`.`orden_tiene_productos` (
+  `orden_id` INT UNSIGNED NOT NULL,
+  `orden_usuarios_id` INT UNSIGNED NOT NULL,
+  `productos_id` INT UNSIGNED NOT NULL,
+  `cantidad` INT NOT NULL,
+  PRIMARY KEY (`orden_id`, `orden_usuarios_id`, `productos_id`),
+  INDEX `fk_orden_has_productos_productos1_idx` (`productos_id` ASC) VISIBLE,
+  INDEX `fk_orden_has_productos_orden1_idx` (`orden_id` ASC, `orden_usuarios_id` ASC) VISIBLE,
+  CONSTRAINT `fk_orden_has_productos_orden1`
+    FOREIGN KEY (`orden_id` , `orden_usuarios_id`)
+    REFERENCES `joyadelcaribe`.`orden` (`id` , `usuarios_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_productos_ordenCompras1`
-    FOREIGN KEY (`ordenCompras_idordenCompras` , `ordenCompras_usuarios_idusuarios`)
-    REFERENCES `JoyaDelCaribe`.`ordenCompras` (`idordenCompras` , `usuarios_idusuarios`)
+  CONSTRAINT `fk_orden_has_productos_productos1`
+    FOREIGN KEY (`productos_id`)
+    REFERENCES `joyadelcaribe`.`productos` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
